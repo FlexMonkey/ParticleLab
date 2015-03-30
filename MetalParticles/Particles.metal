26b183a2cb7b45451bcfce174d449f369bb3eadb
Simon Gladman <simongladman@Simons-MacBook-Pro-2.local>
2015-03-30 11:35:40 +0100

    Got 60fps with two million particles by having four particle definitions per instance of Particle !

--- MetalParticles/Particles.metal
+++ MetalParticles/Particles.metal
@@ -16,6 +16,21 @@ struct Particle
     float positionY;
     float velocityX;
     float velocityY;
+    
+    float positionBX;
+    float positionBY;
+    float velocityBX;
+    float velocityBY;
+    
+    float positionCX;
+    float positionCY;
+    float velocityCX;
+    float velocityCY;
+    
+    float positionDX;
+    float positionDY;
+    float velocityDX;
+    float velocityDY;
 };
 
 kernel void particleRendererShader(texture2d<float, access::write> outTexture [[texture(0)]],
@@ -31,35 +46,113 @@ kernel void particleRendererShader(texture2d<float, access::write> outTexture [[
 {
     const float imageWidth = 1024;
     const Particle inParticle = inParticles[id];
-    const uint2 particlePosition(inParticle.positionX, inParticle.positionY);
+
+    
     
     const int type = id % 3;
 
-    const float3 thisColor = float3(0,0,0); // inTexture.read(particlePosition).rgb;
+    // const float3 thisColor = float3(0,0,0); // inTexture.read(particlePosition).rgb;
     
-    const float4 outColor(thisColor.r + (type != 0 ? 1 : 0.0),
-                          thisColor.g +  (type != 1 ? 1 : 0.0),
-                          thisColor.b + (type != 2 ? 1 : 0.0),
+    const float4 outColor((type != 0 ? 1 : 0.0),
+                          (type != 1 ? 1 : 0.0),
+                          (type != 2 ? 1 : 0.0),
                           1.0);
+    
+    const float massOne = (type == 0 ? 0.42 : (type == 1 ? 0.41 : 0.4));
+    const float massTwo = (type == 0 ? 0.2 : (type == 1 ? 0.19 : 0.18));
+    
+    const uint2 particlePosition(inParticle.positionX, inParticle.positionY);
     
     if (particlePosition.x > 0 && particlePosition.y > 0 && particlePosition.x < imageWidth && particlePosition.y < imageWidth)
     {
         outTexture.write(outColor, particlePosition);
     }
    
-    const float dist = fast::distance(float2(inParticle.positionX, inParticle.positionY), float2(inGravityWell.positionX, inGravityWell.positionY));
-    const float distTwo = fast::distance(float2(inParticle.positionX, inParticle.positionY), float2(inGravityWellTwo.positionX, inGravityWellTwo.positionY));
+    const float2 particlePositionFloat(inParticle.positionX, inParticle.positionY);
     
-    const float factor = (1 / dist) * (type == 0 ? 0.4 : (type == 1 ? 0.39 : 0.38));
-    const float factorTwo = (1 / distTwo) * (type == 0 ? 0.2 : (type == 1 ? 0.19 : 0.18));
-   
+    const float dist = fast::distance(particlePositionFloat, float2(inGravityWell.positionX, inGravityWell.positionY));
+    const float distTwo = fast::distance(particlePositionFloat, float2(inGravityWellTwo.positionX, inGravityWellTwo.positionY));
+    
+    const float factor = (1 / dist) * massOne;
+    const float factorTwo = (1 / distTwo) * massTwo;
+
+    // ---
+    
+    const uint2 particlePositionB(inParticle.positionBX, inParticle.positionBY);
+    
+    if (particlePositionB.x > 0 && particlePositionB.y > 0 && particlePositionB.x < imageWidth && particlePositionB.y < imageWidth)
+    {
+        outTexture.write(outColor, particlePositionB);
+    }
+    
+    const float2 particlePositionBFloat(inParticle.positionBX, inParticle.positionBY);
+    
+    const float distB = fast::distance(particlePositionBFloat, float2(inGravityWell.positionX, inGravityWell.positionY));
+    const float distBTwo = fast::distance(particlePositionBFloat, float2(inGravityWellTwo.positionX, inGravityWellTwo.positionY));
+    
+    const float factorB = (1 / distB) * massOne;
+    const float factorBTwo = (1 / distBTwo) * massTwo;
+    
+    // ---
+
+    
+    const uint2 particlePositionC(inParticle.positionCX, inParticle.positionCY);
+    
+    if (particlePositionC.x > 0 && particlePositionC.y > 0 && particlePositionC.x < imageWidth && particlePositionC.y < imageWidth)
+    {
+        outTexture.write(outColor, particlePositionC);
+    }
+    
+    const float2 particlePositionCFloat(inParticle.positionCX, inParticle.positionCY);
+    
+    const float distC = fast::distance(particlePositionCFloat, float2(inGravityWell.positionX, inGravityWell.positionY));
+    const float distCTwo = fast::distance(particlePositionCFloat, float2(inGravityWellTwo.positionX, inGravityWellTwo.positionY));
+    
+    const float factorC = (1 / distC) * massOne;
+    const float factorCTwo = (1 / distCTwo) * massTwo;
+    
+    // ---
+    
+    
+    const uint2 particlePositionD(inParticle.positionDX, inParticle.positionDY);
+    
+    if (particlePositionD.x > 0 && particlePositionD.y > 0 && particlePositionD.x < imageWidth && particlePositionD.y < imageWidth)
+    {
+        outTexture.write(outColor, particlePositionD);
+    }
+    
+    const float2 particlePositionDFloat(inParticle.positionDX, inParticle.positionDY);
+    
+    const float distD = fast::distance(particlePositionDFloat, float2(inGravityWell.positionX, inGravityWell.positionY));
+    const float distDTwo = fast::distance(particlePositionDFloat, float2(inGravityWellTwo.positionX, inGravityWellTwo.positionY));
+    
+    const float factorD = (1 / distD) * massOne;
+    const float factorDTwo = (1 / distDTwo) * massTwo;
+    
+    // ---
+    
     outParticles[id] = Particle {
-        .velocityX =  (inParticle.velocityX * 0.998) + ((inGravityWell.positionX - inParticle.positionX) * factor) - ((inGravityWellTwo.positionX - inParticle.positionX) * factorTwo),
-        .velocityY =  (inParticle.velocityY * 0.998) + ((inGravityWell.positionY - inParticle.positionY) * factor) - ((inGravityWellTwo.positionY - inParticle.positionY) * factorTwo),
+        
+        .velocityX =  (inParticle.velocityX * 0.995) + ((inGravityWell.positionX - inParticle.positionX) * factor) - ((inGravityWellTwo.positionX - inParticle.positionX) * factorTwo),
+        .velocityY =  (inParticle.velocityY * 0.995) + ((inGravityWell.positionY - inParticle.positionY) * factor) - ((inGravityWellTwo.positionY - inParticle.positionY) * factorTwo),
         .positionX =  inParticle.positionX + inParticle.velocityX,
-        .positionY =  inParticle.positionY + inParticle.velocityY};
+        .positionY =  inParticle.positionY + inParticle.velocityY,
     
-    
+        .velocityBX =  (inParticle.velocityBX * 0.995) + ((inGravityWell.positionX - inParticle.positionBX) * factorB) - ((inGravityWellTwo.positionX - inParticle.positionBX) * factorBTwo),
+        .velocityBY =  (inParticle.velocityBY * 0.995) + ((inGravityWell.positionY - inParticle.positionBY) * factorB) - ((inGravityWellTwo.positionY - inParticle.positionBY) * factorBTwo),
+        .positionBX =  inParticle.positionBX + inParticle.velocityBX,
+        .positionBY =  inParticle.positionBY + inParticle.velocityBY,
+        
+        .velocityCX =  (inParticle.velocityCX * 0.995) + ((inGravityWell.positionX - inParticle.positionCX) * factorC) - ((inGravityWellTwo.positionX - inParticle.positionCX) * factorCTwo),
+        .velocityCY =  (inParticle.velocityCY * 0.995) + ((inGravityWell.positionY - inParticle.positionCY) * factorC) - ((inGravityWellTwo.positionY - inParticle.positionCY) * factorCTwo),
+        .positionCX =  inParticle.positionCX + inParticle.velocityCX,
+        .positionCY =  inParticle.positionCY + inParticle.velocityCY,
+        
+        .velocityDX =  (inParticle.velocityDX * 0.995) + ((inGravityWell.positionX - inParticle.positionDX) * factorD) - ((inGravityWellTwo.positionX - inParticle.positionDX) * factorDTwo),
+        .velocityDY =  (inParticle.velocityDY * 0.995) + ((inGravityWell.positionY - inParticle.positionDY) * factorD) - ((inGravityWellTwo.positionY - inParticle.positionDY) * factorDTwo),
+        .positionDX =  inParticle.positionDX + inParticle.velocityDX,
+        .positionDY =  inParticle.positionDY + inParticle.velocityDY
+        };
     
     // ----
     /*
--- MetalParticles/ViewController.swift
+++ MetalParticles/ViewController.swift
@@ -15,7 +15,7 @@ import CoreData
 class ViewController: UIViewController
 {
     let imageSide: UInt = 1024
- 
+    
     let bytesPerRow = UInt(4 * 1024)
     
     var kernelFunction: MTLFunction!
@@ -23,37 +23,44 @@ class ViewController: UIViewController
     var defaultLibrary: MTLLibrary! = nil
     var device: MTLDevice! = nil
     var commandQueue: MTLCommandQueue! = nil
-
+    
     let metalLayer = CAMetalLayer()
     
     var region: MTLRegion = MTLRegionMake2D(0, 0, Int(1024), Int(1024))
- 
+    
     let blankBitmapRawData = [UInt8](count: Int(1024 * 1024 * 4), repeatedValue: 0)
     
     var errorFlag:Bool = false
- 
+    
     var particle_threadGroupCount:MTLSize!
     var particle_threadGroups:MTLSize!
     
-    let particleCount: Int = 2097152 // 4194304 2097152   1048576
+    let particleCount: Int = 524288 // 4194304 2097152   1048576  524288
     var particlesMemory:UnsafeMutablePointer<Void> = nil
     let alignment:UInt = 0x4000
-    let particlesMemoryByteSize:UInt = UInt(2097152) * UInt(sizeof(Particle))
+    let particlesMemoryByteSize:UInt = UInt(524288) * UInt(sizeof(Particle))
     var particlesVoidPtr: COpaquePointer!
     var particlesParticlePtr: UnsafeMutablePointer<Particle>!
     var particlesParticleBufferPtr: UnsafeMutableBufferPointer<Particle>!
     
     var gravityWellAngle: Float = 0.0
-    var gravityWellParticle = Particle(positionX: 512, positionY: 512, velocityX: 0, velocityY: 0)
     
-    var gravityWellParticleTwo = Particle(positionX: 512, positionY: 512, velocityX: 0, velocityY: 0)
+    var gravityWellParticle = Particle(positionX: 512, positionY: 512, velocityX: 0, velocityY: 0,
+        positionBX: 512, positionBY: 512, velocityBX: 0, velocityBY: 0,
+        positionCX: 512, positionCY: 512, velocityCX: 0, velocityCY: 0,
+        positionDX: 512, positionDY: 512, velocityDX: 0, velocityDY: 0)
+    
+    var gravityWellParticleTwo = Particle(positionX: 512, positionY: 512, velocityX: 0, velocityY: 0,
+        positionBX: 512, positionBY: 512, velocityBX: 0, velocityBY: 0,
+        positionCX: 512, positionCY: 512, velocityCX: 0, velocityCY: 0,
+        positionDX: 512, positionDY: 512, velocityDX: 0, velocityDY: 0)
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.blackColor()
-
+        
         view.layer.addSublayer(metalLayer)
         
         metalLayer.framebufferOnly = false
@@ -72,35 +79,65 @@ class ViewController: UIViewController
         particlesVoidPtr = COpaquePointer(particlesMemory)
         particlesParticlePtr = UnsafeMutablePointer<Particle>(particlesVoidPtr)
         particlesParticleBufferPtr = UnsafeMutableBufferPointer(start: particlesParticlePtr, count: particleCount)
- 
+        
         for index in particlesParticleBufferPtr.startIndex ..< particlesParticleBufferPtr.endIndex
         {
             var positionX = Float(arc4random() % UInt32(imageSide))
             var positionY = Float(arc4random() % UInt32(imageSide))
             let velocityX = (Float(arc4random() % 10) - 5) / 10.0
             let velocityY = (Float(arc4random() % 10) - 5) / 10.0
-     
+            
+            var positionBX = Float(arc4random() % UInt32(imageSide))
+            var positionBY = Float(arc4random() % UInt32(imageSide))
+            let velocityBX = (Float(arc4random() % 10) - 5) / 10.0
+            let velocityBY = (Float(arc4random() % 10) - 5) / 10.0
+            
+            var positionCX = Float(arc4random() % UInt32(imageSide))
+            var positionCY = Float(arc4random() % UInt32(imageSide))
+            let velocityCX = (Float(arc4random() % 10) - 5) / 10.0
+            let velocityCY = (Float(arc4random() % 10) - 5) / 10.0
+            
+            var positionDX = Float(arc4random() % UInt32(imageSide))
+            var positionDY = Float(arc4random() % UInt32(imageSide))
+            let velocityDX = (Float(arc4random() % 10) - 5) / 10.0
+            let velocityDY = (Float(arc4random() % 10) - 5) / 10.0
+            
             let positionRule = Int(arc4random() % 4)
             
             if positionRule == 0
             {
                 positionX = 0
+                positionBX = 0
+                positionCX = 0
+                positionDX = 0
             }
             else if positionRule == 1
             {
                 positionX = Float(imageSide)
+                positionBX = Float(imageSide)
+                positionCX = Float(imageSide)
+                positionDX = Float(imageSide)
             }
             else if positionRule == 2
             {
                 positionY = 0
+                positionBY = 0
+                positionCY = 0
+                positionDY = 0
             }
             else
             {
                 positionY = Float(imageSide)
+                positionBY = Float(imageSide)
+                positionCY = Float(imageSide)
+                positionDY = Float(imageSide)
             }
-
-            let particle = Particle(positionX: positionX, positionY: positionY, velocityX: velocityX, velocityY: velocityY)
-    
+            
+            let particle = Particle(positionX: positionX, positionY: positionY,velocityX: velocityX, velocityY: velocityY,
+                positionBX: positionBX, positionBY: positionBY, velocityBX: velocityBX, velocityBY: velocityBY,
+                positionCX: positionCX, positionCY: positionCY, velocityCX: velocityCX, velocityCY: velocityCY,
+                positionDX: positionDX, positionDY: positionDY, velocityDX: velocityDX, velocityDY: velocityDY)
+            
             particlesParticleBufferPtr[index] = particle
         }
     }
@@ -112,7 +149,7 @@ class ViewController: UIViewController
         device = MTLCreateSystemDefaultDevice()
         
         metalLayer.device = device
- 
+        
         if device == nil
         {
             errorFlag = true
@@ -129,10 +166,10 @@ class ViewController: UIViewController
             
             defaultLibrary = device.newDefaultLibrary()
             commandQueue = device.newCommandQueue()
-   
+            
             particle_threadGroupCount = MTLSize(width:32,height:1,depth:1)
             particle_threadGroups = MTLSize(width:(particleCount + 31) / 32, height:1, depth:1)
- 
+            
             kernelFunction = defaultLibrary.newFunctionWithName("particleRendererShader")
             pipelineState = device.newComputePipelineStateWithFunction(kernelFunction!, error: nil)
             
@@ -141,7 +178,7 @@ class ViewController: UIViewController
             run()
         }
     }
-
+    
     var frameStartTime: CFAbsoluteTime!
     var frameNumber = 0
     
@@ -160,16 +197,18 @@ class ViewController: UIViewController
         }
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
-        {
-            self.applyShader()
-            dispatch_async(dispatch_get_main_queue(),
             {
-                self.run();
-            });
+                self.applyShader()
+                dispatch_async(dispatch_get_main_queue(),
+                    {
+                        self.run();
+                });
         });
     }
     
     var imageBytes = [UInt8](count: Int(1024 * 1024 * 4), repeatedValue: 0)
+    
+    let particleSize = sizeof(Particle)
     
     final func applyShader()
     {
@@ -177,7 +216,7 @@ class ViewController: UIViewController
         let commandEncoder = commandBuffer.computeCommandEncoder()
         
         commandEncoder.setComputePipelineState(pipelineState)
-
+        
         let particlesBufferNoCopy = device.newBufferWithBytesNoCopy(particlesMemory, length: Int(particlesMemoryByteSize),
             options: nil, deallocator: nil)
         
@@ -187,14 +226,14 @@ class ViewController: UIViewController
         gravityWellAngle += 0.06
         gravityWellParticle.positionX = 512 + 65 * sin(gravityWellAngle)
         gravityWellParticle.positionY = 512 + 65 * cos(gravityWellAngle)
-
+        
         gravityWellParticle.positionX = 512 + 15 * sin(0 - gravityWellAngle / 2)
         gravityWellParticleTwo.positionY = 512 + 15 * cos(0 - gravityWellAngle / 2)
         
-        var inGravityWell = device.newBufferWithBytes(&gravityWellParticle, length: sizeofValue(gravityWellParticle), options: nil)
+        var inGravityWell = device.newBufferWithBytes(&gravityWellParticle, length: particleSize, options: nil)
         commandEncoder.setBuffer(inGravityWell, offset: 0, atIndex: 2)
-
-        var inGravityWellTwo = device.newBufferWithBytes(&gravityWellParticleTwo, length: sizeofValue(gravityWellParticle), options: nil)
+        
+        var inGravityWellTwo = device.newBufferWithBytes(&gravityWellParticleTwo, length: particleSize, options: nil)
         commandEncoder.setBuffer(inGravityWellTwo, offset: 0, atIndex: 3)
         
         let drawable = metalLayer.nextDrawable()
@@ -205,16 +244,16 @@ class ViewController: UIViewController
             let blitCommandBuffer = commandQueue.commandBuffer()
             let blitCommandEncoder = blitCommandBuffer.blitCommandEncoder()
             blitCommandEncoder.copyFromTexture(drawable.texture, sourceSlice: 0, sourceLevel: 0, sourceOrigin: MTLOrigin(x: 0, y: 0, z: 0), sourceSize: MTLSize(width: 1024, height: 1024, depth: 1), toTexture: copiedTexture, destinationSlice: 0, destinationLevel: 0, destinationOrigin: MTLOrigin(x: 0, y: 0, z: 0))
-
+            
             blitCommandEncoder.endEncoding()
             */
             
             // works, but is slow - use blitCommandEncoder()
-           /*
+            /*
             drawable.texture.getBytes(&imageBytes, bytesPerRow: Int(bytesPerRow), fromRegion: region, mipmapLevel: 0)
             copiedTexture.replaceRegion(self.region, mipmapLevel: 0, withBytes: imageBytes, bytesPerRow: Int(bytesPerRow))
-           */
-
+            */
+            
             
             drawable.texture.replaceRegion(self.region, mipmapLevel: 0, withBytes: blankBitmapRawData, bytesPerRow: Int(bytesPerRow))
             commandEncoder.setTexture(drawable.texture, atIndex: 0)
@@ -222,7 +261,7 @@ class ViewController: UIViewController
             /*
             commandEncoder.setTexture(copiedTexture, atIndex: 1)
             */
-
+            
             commandEncoder.dispatchThreadgroups(particle_threadGroups, threadsPerThreadgroup: particle_threadGroupCount)
             
             commandEncoder.endEncoding()
@@ -239,16 +278,16 @@ class ViewController: UIViewController
             
             println("metalLayer.nextDrawable() returned nil")
         }
-
+        
     }
-
+    
     override func viewDidLayoutSubviews()
     {
         if view.frame.height > view.frame.width
         {
-           let imageSide = view.frame.width
+            let imageSide = view.frame.width
             
-           metalLayer.frame = CGRect(x: 0, y: view.frame.height / 2.0 - imageSide / 2, width: imageSide, height: imageSide).rectByInsetting(dx: -1, dy: 01)
+            metalLayer.frame = CGRect(x: 0, y: view.frame.height / 2.0 - imageSide / 2, width: imageSide, height: imageSide).rectByInsetting(dx: -1, dy: 01)
         }
         else
         {
@@ -263,7 +302,7 @@ class ViewController: UIViewController
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
 
 struct Particle
@@ -272,5 +311,20 @@ struct Particle
     var positionY: Float = 0
     var velocityX: Float = 0
     var velocityY: Float = 0
+    
+    var positionBX: Float = 0
+    var positionBY: Float = 0
+    var velocityBX: Float = 0
+    var velocityBY: Float = 0
+    
+    var positionCX: Float = 0
+    var positionCY: Float = 0
+    var velocityCX: Float = 0
+    var velocityCY: Float = 0
+    
+    var positionDX: Float = 0
+    var positionDY: Float = 0
+    var velocityDX: Float = 0
+    var velocityDY: Float = 0
 }
 
